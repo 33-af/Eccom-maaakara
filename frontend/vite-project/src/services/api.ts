3afb248012d77deeb3bfb5602dd8441675a1408f
Mykhail <gorgefraggggg@gmail.com>
2024-11-19 23:08:53 +0300

    fix bag

--- frontend/vite-project/src/services/api.ts
+++ frontend/vite-project/src/services/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react';
 import { RootState } from '../redux/store/store';
 
 const baseQuery = fetchBaseQuery({
-   baseUrl: 'https://eccom-maaakara-backend.onrender.com//api',
+   baseUrl: 'https://eccom-maaakara-backend.onrender.com/api',
     prepareHeaders: (headers, { getState }) => {
         const token = (getState() as RootState).auth.user?.token || localStorage.getItem("token");
         console.log("Token from getState or localStorage:", token);  
