b82e80e9caf6c7a24cd0afdbc72f9cb702f9fc7c
Mykhailo Petrenko <140710486+33-af@users.noreply.github.com>
2024-11-19 21:24:15 +0300

    Update Sausages.tsx

--- frontend/vite-project/src/components/MainPageComponents/ProductList/Sausages.tsx
+++ frontend/vite-project/src/components/MainPageComponents/ProductList/Sausages.tsx
@@ -26,7 +26,7 @@ const Sausages: React.FC = () => {
                     className={styles.productList__card}
                 >
                     <img
-                        src={`http://localhost:5001${sausage.image}`}
+                        src={`https://ecc-mak-backend.onrender.com/${sausage.image}`}
                         alt={sausage.title}
                         className={styles.productImage}
                     />
