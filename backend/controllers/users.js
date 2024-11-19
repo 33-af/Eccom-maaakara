98a104a316706d95652864b0a1cc2b5be30914ae
Mykhailo Petrenko <140710486+33-af@users.noreply.github.com>
2024-11-19 21:53:30 +0300

    Update users.js

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -3,38 +3,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 
-// const login = async (req, res) => {
-//     try {
-//         const { email, password, name } = req.body;
 
-//         if (!email || !password || !name) {
-//             return res.status(400).json({ message: "Please fill in all required fields" });
-//         }
-
-//         // Дополнительная логика проверки email и password
-//         const user = await prisma.user.findFirst({
-//             where: {
-//                 email,
-//             }
-//         });
-//         const secret = process.env.SECRET_KEY;
-//         const isPasswordCorrect = user && (await bcrypt.compare(password, user.password));
-
-//         if (user && isPasswordCorrect) {
-//             return res.status(200).json({
-//                 id: user.id,
-//                 email: user.email,
-//                 name: user.name,
-//                 token: jwt.sign({ id: user.id }, secret, { expiresIn: '30d' })
-//             });
-//         } else {
-//             return res.status(400).json({ message: 'Неверно введён логин или пароль' });
-//         }
-//     } catch (error) {
-//         console.error(error);
-//         return res.status(500).json({ message: "Something went wrong" });
-//     }
-// };
 
 
 const register = async (req, res) => {
