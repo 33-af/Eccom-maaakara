8dc2a7ae20f2e02cdeeca6b676dea5079ae25559
Mykhail <gorgefraggggg@gmail.com>
2024-11-20 21:56:06 +0300

    qewqewqeqw232213edasasdasd

--- frontend/vite-project/src/components/MainPageComponents/Sidebar/Sidebar.tsx
+++ frontend/vite-project/src/components/MainPageComponents/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState } from 'react';
 import styles from './style.module.css';
 import { useGetBannersQuery } from '../../../services/products';
 import { Link } from 'react-router-dom';
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 const Sidebar: React.FC = () => {
     const { data: banners, error, isLoading } = useGetBannersQuery();
     console.log(banners)
-    const [currentIndex, setCurrentIndex] = useState(0); 
+    const [currentIndex, setCurrentIndex] = useState(0);
     console.log(currentIndex)
 
     if (isLoading) return <p>Loading banners...</p>;
@@ -15,14 +15,14 @@ const Sidebar: React.FC = () => {
 
     const nextBanner = () => {
         if (banners && banners.length > 0) {
-            setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length); 
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length);
         }
     };
 
     const prevBanner = () => {
         if (banners && banners.length > 1) {
             setCurrentIndex(
-                (prevIndex) => (prevIndex - 1 + banners.length) % banners.length 
+                (prevIndex) => (prevIndex - 1 + banners.length) % banners.length
             );
         }
     };
@@ -46,16 +46,16 @@ const Sidebar: React.FC = () => {
                             ←
                         </button>
 
-                        {currentBanner ? (
-                            <Link to={`/admin-panel-one-banner/${currentBanner?.id}`}>
-                            <img
-                                src={currentBanner.imageUrl}
-                                alt="Banner"
-                                className={styles.bannerImage}
-                            />
-                        </Link>
+                        {currentBanner && currentBanner.imageUrl ? (
+                            <Link to={`/admin-panel-one-banner/${currentBanner.id}`}>
+                                <img
+                                    src={currentBanner.imageUrl}
+                                    alt="Banner"
+                                    className={styles.bannerImage}
+                                />
+                            </Link>
                         ) : (
-                            <p>No banners available</p> 
+                            <p>No banners available</p>
                         )}
                         <button onClick={nextBanner} className={styles.arrowButton}>
                             →
